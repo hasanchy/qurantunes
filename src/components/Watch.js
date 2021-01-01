a67e3adfb693e7c1938bfe7dfa8748ebee9b74a4
Hasan Chowdhury <hasan.chowdhury@customergauge.com>
2021-01-01 05:31:22 +0100

    Add background image

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import db from '../db.json';
+import './Watch.css';
 
 function Watch({ id }) {
     let audio;
@@ -25,7 +26,13 @@ function Watch({ id }) {
     return (
       <>
         {id ? (
-          <div style={{backgroundColor:"#000", color:"#fff", height:"300px", padding:"10px"}}>
+        <div id="animate-area">
+            <div style={{width:"100%", height:"100%", backgroundColor:"rgb(0, 0, 0, .7)", color:"#fff", textAlign:"center", fontSize:"14", fontWeight:"bold",}}>
+                <span style={{display:"inline-block",padding:"20px"}}>
+                    <div style={{fontSize:"30px", marginTop:"30px"}}>تَبَارَكَ الَّذِي بِيَدِهِ الْمُلْكُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيرٌ</div>
+                    <div  style={{fontSize:"22px", marginTop:"30px"}}>Blessed be He in Whose hands is Dominion; and He over all things hath Power.</div>
+                </span>
+            </div>
           </div>
         ) : (
           <></>
