3b1033c5ffb2bec40112aaba04c6d7390bb420f1
Hasan Chowdhury <hasan.chowdhury@customergauge.com>
2021-01-01 01:30:37 +0100

    Update UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  useLocation
+} from "react-router-dom";
 
-function App() {
+// React Router does not have any opinions about
+// how you should parse URL query strings.
+//
+// If you use simple key=value query strings and
+// you do not need to support IE 11, you can use
+// the browser's built-in URLSearchParams API.
+//
+// If your query strings contain array or object
+// syntax, you'll probably need to bring your own
+// query parsing function.
+
+export default function QueryParamsExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <QueryParamsDemo />
+    </Router>
+  );
+}
+
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function QueryParamsDemo() {
+  let query = useQuery();
+
+  return (
+    <div  style={{padding:"30px"}}>
+      <Watch q={query.get("q")} />
+      <Thumbnails/>
     </div>
   );
 }
 
-export default App;
+function Watch({ q }) {
+  return (
+    <>
+      {q ? (
+        <div style={{backgroundColor:"#000", color:"#fff", height:"300px", padding:"10px"}}>
+        <h3>
+          The <code>name</code> in the query string is &quot;{q}
+          &quot;
+        </h3>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+}
+
+function Thumbnails() {
+  return (
+    <div>
+      <div>
+            <Link to="/watch?q=65h4pdw6Bkc">
+              <div style={{border:"1px solid #ddd", padding:"10px"}}>
+              021 - Surah Anbiya
+              </div>
+            </Link>
+          
+          
+            <Link to="/watch?q=4AQR4a9ZZ0I">
+            <div style={{border:"1px solid #ddd", padding:"10px"}}>
+              067 - Surah Mulk
+              </div>
+              </Link>
+          
+          
+            <Link to="/watch?q=oNlJsyklaVk">
+            <div style={{border:"1px solid #ddd", padding:"10px"}}>
+              014 - Surah Ibrahim
+              </div>
+              </Link>
+          
+          
+            <Link to="/watch?q=EBec5wKMu58">
+            <div style={{border:"1px solid #ddd", padding:"10px"}}>
+              072 - Surah Jinn
+              </div>
+              </Link>
+          
+          
+            <Link to="/watch?q=4pfbNgkfPtM">
+            <div style={{border:"1px solid #ddd", padding:"10px"}}>
+              002 - Surah Baqarah
+              </div>
+              </Link>
+      </div>
+    </div>
+  );
+}
