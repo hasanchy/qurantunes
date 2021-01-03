ec3748299a5cab5f874d057957bd1a258f73de8e
Hasan Chowdhury <hasan.chowdhury@customergauge.com>
2021-01-04 00:35:39 +0100

    Add bengali translation

--- src/components/DetailsView.js
+++ src/components/DetailsView.js
@@ -10,7 +10,7 @@
               <div style={{fontSize:"30px", marginTop:"30px"}}>{details.title}</div>
               <div style={{fontSize:"20px", marginTop:"30px"}}>{details.description}</div>
               <div style={{fontSize:"14px", marginTop:"70px"}}>Reciter: {details.reciter}</div>
-              <div style={{fontSize:"12px", marginTop:"8px"}}>Translator: {details.translator}</div>
+              {/* <div style={{fontSize:"12px", marginTop:"8px"}}>Translator: {details.translator}</div> */}
           </span>
       );
     }else{
--- src/components/Subtitle.js
+++ src/components/Subtitle.js
@@ -2,18 +2,18 @@
   function Subtitle(props){
 
     const { duration, curTime, text } = props;
-    let sa, en;
+    let sa, translation;
     for( var t in text ){
       if( t <= curTime ){
         sa = text[t].sa;
-        en = text[t].en;
+        translation = text[t].bn;
       }
     }
 
     return (
         <span style={{display:"inline-block",padding:"20px"}}>
             <div style={{fontSize:"30px", marginTop:"30px"}}>{sa}</div>
-            <div  style={{fontSize:"22px", marginTop:"30px"}}>{en}</div>
+            <div  style={{fontSize:"22px", marginTop:"30px"}}>{translation}</div>
         </span>
     );
   }
